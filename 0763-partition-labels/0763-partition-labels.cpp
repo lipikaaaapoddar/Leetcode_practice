d722df08711965ab8bc90a440c7ab7d0fc49ffca
lipikaaaapoddar <lipikapoddar18@gmail.com>
2026-06-17 10:50:25 +0530

    Time: 0 ms (100%), Space: 8.9 MB (39.81%) - LeetHub

---
+++ 0763-partition-labels/0763-partition-labels.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        unordered_map<char , int> mp;
+        for(int i =0 ; i<s.size(); i++){
+            mp[s[i]]=i;
+        }
+        vector<int> ans;
+        int start = 0;
+        int end =0;
+        for(int i =0 ; i<s.size(); i++){
+            end = max(end, mp[s[i]]);
+            if(end == i){
+                ans.push_back(end - start +1);
+                start = i+1;
+            }
+        }
+        return ans;
+    }
+};
