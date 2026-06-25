46380d3f1dc9f6a3a85d32995e84139a88ede4dc
lipikaaaapoddar <lipikapoddar18@gmail.com>
2026-06-25 22:50:29 +0530

    Time: 25 ms (7.7%), Space: 45.9 MB (38.14%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(), people.end());
+        int start =0 , boat =0;
+        int end = people.size()-1;
+        while(start<= end){
+            if(people[start]+ people[end] <= limit){
+                start++;
+                end--;
+            } 
+            else end--;
+            boat ++;
+        }
+        return boat;
+    }
+};
