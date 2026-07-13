352c826553969dd8d16e4b1021a8749322122336
Lipika Poddar <lipikapoddar18@gmail.com>
2026-07-13 23:31:08 +0530

    Time: 0 ms (100%), Space: 12.3 MB (94.39%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        int a = *max_element(candies.begin(),candies.end());
+        vector<bool> ans(candies.size(),false);
+        for(int i=0; i< candies.size(); i++){
+            if(candies[i]+extraCandies >=a) ans[i]=true;
+        }
+        return ans;
+    }
+};
