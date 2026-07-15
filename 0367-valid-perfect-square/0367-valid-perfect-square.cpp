c9264f88be1a5b941731c5f42ff8b55f41801619
Lipika Poddar <lipikapoddar18@gmail.com>
2026-07-15 18:35:35 +0530

    Time: 0 ms (100%), Space: 7.7 MB (53.93%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        long long i =1; 
+        long long j = num;
+        while(i<=j){
+            long long mid = i + (j-i)/2;
+            if(mid*mid == num) return true;
+            else if (mid* mid > num){
+                j = mid-1;
+            }
+            else i = mid+1;
+        }
+        return false;
+    }
+};
