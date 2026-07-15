b8d244eea65a0230bc94fc2ed79192a5506459aa
Lipika Poddar <lipikapoddar18@gmail.com>
2026-07-15 18:54:32 +0530

    Time: 0 ms (100%), Space: 15.4 MB (16.27%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
@@ -3,10 +3,39 @@ public:
     vector<int> targetIndices(vector<int>& nums, int target) {
         sort(nums.begin(),nums.end());
         vector<int> ans;
-        for(int i =0 ; i<nums.size(); i++){
-            if(nums[i]== target){
-                ans.push_back(i);
+
+        int i =0 , j = nums.size()-1 , first=-1;
+        while(i<=j){
+            int mid = i + (j-i)/2;
+            if(nums[mid]== target){
+               first = mid;
+               j = mid-1;
             }
+            else if(nums[mid]<target){
+                i=mid+1;
+            }
+            else j = mid-1;
+        }
+        
+        if(first ==-1) return {};
+
+        i =0 , j = nums.size()-1 ;
+        int last=-1;
+        while(i<=j){
+            int mid = i + (j-i)/2;
+            if(nums[mid]== target){
+               last = mid;
+               i = mid+1;
+            }
+            else if(nums[mid]<target){
+                i=mid+1;
+            }
+            else j = mid-1;
+        }
+
+        while(first<= last){
+            ans.push_back(first);
+            first++;
         }
         return ans;
     }
