a84b2b3304f581130b564ce741a28d8f017b9726
Lipika Poddar <lipikapoddar18@gmail.com>
2026-07-26 21:21:33 +0530

    Time: 0 ms (100%), Space: 8.6 MB (89.61%) - LeetHub

---
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProduct(int n) {
+        int l =0 , sl =0;
+        while(n>0){
+            int d = n%10;
+            if(d>l) {
+                sl = l;
+                l = d;}
+            else if (d >sl) sl = d;
+            n /=10;
+        }
+        return l*sl;
+    }
+};
