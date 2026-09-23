aca6f5fad141d430b4ea3bc50e517e2b694875c0
Lipika Poddar <lipikapoddar18@gmail.com>
2026-09-23 17:07:28 +0530

    Time: 11 ms (42.43%), Space: 12 MB (10.87%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,16 +9,14 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        ListNode* slow = head;
         ListNode* fast = head;
-        while(fast !=NULL && fast->next!=NULL){
+        ListNode* slow = head;
+        while(fast!=NULL && fast->next !=NULL ){
             slow = slow->next;
-            fast=fast->next->next;
-            // not by value but by node
-            if(slow==fast){
-                return true;
-            }
+            fast = fast->next->next;
+            if (fast == slow) return true;
         }
         return false;
+
     }
 };
