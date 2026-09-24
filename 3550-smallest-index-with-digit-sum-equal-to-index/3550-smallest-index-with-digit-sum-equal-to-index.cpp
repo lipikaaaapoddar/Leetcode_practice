009e00a59a13e37e41348308153ec33431a0df51
Lipika Poddar <lipikapoddar18@gmail.com>
2026-09-24 19:06:33 +0530

    Time: 0 ms (100%), Space: 30.8 MB (98.04%) - LeetHub

---
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int smallestIndex(vector<int>& nums) {
+        int ans = INT_MAX;
+        for(int i =0 ; i<nums.size(); i++){
+            int sum =0;
+            int n = nums[i];
+            while(n>0){
+                sum += n%10;
+                n /=10;
+            }
+            if(sum == i) ans = min(sum , ans);
+        }
+        return ans!= INT_MAX? ans : -1;
+    }
+};
